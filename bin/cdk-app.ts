27757723770c7ba8d2dedae44f3f87373fd29bc3
PhillipNinan <ninan.phillip@gmail.com>
2021-03-29 14:46:25 -0400

    add vpc to base

--- bin/cdk-app.ts
+++ bin/cdk-app.ts
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
 import { Construct } from '@aws-cdk/core';
-import { CdkBaseStack } from '../lib/cdk-base-stack/cdk-base-stack';
+import { CdkBaseStack, CdkBaseStackProps } from '../lib/cdk-base-stack/cdk-base-stack';
 
 class DemoApp extends Construct{
-    constructor(scope: Construct, id:string){
+    constructor(scope: cdk.App, id:string){
         super(scope,id);
-        const base:CdkBaseStack = new CdkBaseStack(this, `CdkVpcStack-${id}`);
+        // create a small vpc for the demo
+        const vpcProps: CdkBaseStackProps = {
+            stage: id, 
+            description: "Base VPC for AWS Cloud Application",
+            tags: {env: id},
+            stackName: `PublicVpcStack-${id}` // used in cloudformation for naming stack
+        }        
+
+        const base:CdkBaseStack = new CdkBaseStack(this, `CdkVpcStack-${id}`,
+            vpcProps);
     }
 }
 const app = new cdk.App();
--- lib/cdk-base-stack/cdk-base-stack.ts
+++ lib/cdk-base-stack/cdk-base-stack.ts
@@ -1,8 +1,25 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+
+export interface CdkBaseStackProps extends cdk.StackProps {
+  stage: string;
+}
 
 export class CdkBaseStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  private readonly vpc:ec2.Vpc;
+  constructor(scope: cdk.Construct, id: string, props?: CdkBaseStackProps) {
     super(scope, id, props);
 
+    // create a VPC with no private subnets. 
+    // this is for our demo purpose as this will be cheaper since you do not need a nat gateway
+    const vpc = new ec2.Vpc(this, `VPC-${props?.stage}`, {
+      natGateways:0,
+      maxAzs: 2,
+    });    
+
+    this.vpc = vpc;
   }
+  get stackVpc() : ec2.Vpc{
+    return this.vpc;
+  }  
 }
